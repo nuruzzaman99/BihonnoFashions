82c29503651562ba7ed0884434e18d5652872436
nuruzzaman99 <nuruzzamanmoyem.99@gmail.com>
2022-08-14 01:17:15 +0600

    version2

--- BihonnoFashions/settings.py
+++ BihonnoFashions/settings.py
@@ -12,7 +12,6 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 
 import os
 from pathlib import Path
-from django import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -27,7 +26,7 @@ SECRET_KEY = 'django-insecure-#pmvt-$_7opf-lniq3fl+)%@ykqqwvk9bc8$x1jo6_(+xqo6!%
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['0.0.0.0:5000']
 
 
 # Application definition
@@ -125,8 +124,6 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static')
 ]
-
-django_heroku.settings(locals())
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
