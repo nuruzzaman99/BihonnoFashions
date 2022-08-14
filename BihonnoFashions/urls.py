38d4cf0bd33e3ba7c5b1d3873c8c07237633a6d1
nuruzzaman99 <nuruzzamanmoyem.99@gmail.com>
2022-08-14 08:35:48 +0600

    add static files

--- BihonnoFashions/urls.py
+++ BihonnoFashions/urls.py
@@ -26,4 +26,4 @@ urlpatterns = [
     path('cart/', include('Cart.urls')),
     path('product/', include('Product.urls')),
     
-]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) + static(settings.STATIC_URL, document_root = settings.STATIC_ROOT)
